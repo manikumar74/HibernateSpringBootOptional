a23fad35f3cb1d125ac2f53f2046d026c0a7c361
manikumar74 <38670628+manikumar74@users.noreply.github.com>
2020-10-21 16:40:48 -0500

    springboot test

---
+++ src/test/java/com/baeldung/cicd/CiCdApplicationIntegrationTest.java
@@ -0,0 +1,15 @@
+package com.baeldung.cicd;
+
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationIntegrationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
